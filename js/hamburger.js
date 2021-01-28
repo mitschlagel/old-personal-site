c032cde00bca4e73f60602ed0e6f138f93ae8033
Spencer Jones <spencerjones@creighton.edu>
2021-01-28 16:37:53 -0600

    Latest build to test online

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
+
+    // Toggle hamburger menu on click
     $('.single-stripe').click(function () {
         $(this).toggleClass('animate');
-        $('.mobile-nav').toggleClass('open');
+        $('.small-nav').toggleClass('open');
+    });
+
+    // Close menu when link is clicked
+    $('.small-nav').click(function () {
+        $('.single-stripe').toggleClass('animate');
+        $(this).removeClass('open');
     });
 
 
